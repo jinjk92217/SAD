c9d1aa057a94f4e403e1332c59938a8079ac51b0
Dongyang <charlieyoung.cy@gmail.com>
2016-04-21 14:11:18 +0200

    update PRAAG Detector

--- Config_StreamDetector.py
+++ Config_StreamDetector.py
@@ -5,6 +5,7 @@ email: jiakun@kth.se
 """
 from Stream_AnomalyDetector.DDM import DDM
 from Stream_AnomalyDetector.CUSUM import CUSUM
+from Stream_AnomalyDetector.PRAAG import PRAAG
 
 __author__ = 'jiakun'
 
@@ -30,17 +31,19 @@ def CUSUM_StreamDetector(drift = 1, threshold = 1):
     '''
     return CUSUM(drift,threshold)
 
-def PRAAG_StreamDetector(drift = 1, threshold = 1):
+
+def PRAAG_StreamDetector(r = 100,  l = 1000,  e = 0.0001,  a = 15, k=0.05):
     '''
 
-    :param drift: The drift of the score that could be detected
-    e.g. 1.0
-    :param threshold: the threshold that during some period the positive and negative changes exceeds the threshold
-    e.g. 10.0
+    :param r: Size of recent window(small one)
+    :param l: Size of reference window(large one)
+    :param e: precision, needed only if using GK quantile estimation
+    :param a: thresthold of negative log probability value
+    :param k: thresthold percentage of considering a window abnormal
     :return:
 
     '''
-    return CUSUM(drift,threshold)    
+    return PRAAG(r,l,e,a,k)
 
 
 
---
+++ Stream_AnomalyDetector/PRAAG.py
@@ -0,0 +1,17 @@
+from Stream_Detector import Stream_Detector
+import ctypes
+
+
+class PRAAG(Stream_Detector):
+    def __init__(self, r = 100,  l = 1000,  e = 0.0001,  a = 14.0, k=0.05):
+        Stream_Detector.__init__(self)
+        self.Detector = ctypes.CDLL("./Stream_AnomalyDetector/C++/PRAAG.so")
+        self.Detector.Process.argtypes = [ctypes.c_double]
+        self.Detector.init.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double]
+        # self.Detector.Process.restype = ctypes.POINTER(ctypes.c_double)
+        self.Detector.init(r,l,e,a, k)
+
+
+    def check(self,score):
+        Stream_Detector.check(self)
+        return self.Detector.Process(score)
